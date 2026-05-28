4695f1f76a1107629e4d5dedc0696a7b02aafc81
Tran Giap <trangiap308@gmail.com>
2026-05-28 17:37:18 +0700

    Update config

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,12 @@
 import 'dotenv/config';
 import { SOROBAN_RPC_URL, HORIZON_URL, CHAIN_IDS, NETWORK_PASSPHRASE } from './common/chains';
-// export const CFG = {
-//   horizon: process.env.HORIZON_URL!,
-//   rpc: process.env.SOROBAN_RPC_URL!,
-//   startLedger: Number(process.env.START_LEDGER || 1),
-//   gqlPort: Number(process.env.GRAPHQL_PORT || 8080),
-//   databaseUrl: process.env.DATABASE_URL!,
-//   contractIds: (process.env.CONTRACT_IDS || '')
-//     .split(',').map(s => s.trim()).filter(Boolean),
-//   topicFilter: JSON.parse(process.env.TOPIC_FILTER || '[]') as string[][]
-// };
+export const CFG = {
+  gqlPort: Number(process.env.GRAPHQL_PORT || 8080),
+  databaseUrl: process.env.DATABASE_URL!,
+  contractIds: (process.env.CONTRACT_IDS || '')
+    .split(',').map(s => s.trim()).filter(Boolean),
+  topicFilter: JSON.parse(process.env.TOPIC_FILTER || '[]') as string[][]
+};
 
 export type NetworkConfig = {
   name: typeof CHAIN_IDS.STELLAR_MAINNET | typeof CHAIN_IDS.STELLAR_TESTNET;
